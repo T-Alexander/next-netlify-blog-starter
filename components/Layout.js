a710d2551be34b6a632350a6b398f99ffd3c847f
T-Alexander <tomalexander404@gmail.com>
2020-09-24 15:37:26 +0100

    fix ga id

--- components/Layout.js
+++ components/Layout.js
@@ -29,11 +29,11 @@ export default function Layout({ children, pageTitle, description, ...props }) {
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-UA-70072493-5');`}} />
+      })(window,document,'script','dataLayer','UA-70072493-5');`}} />
 
         <title>{pageTitle}</title>        
       </Head>
-      <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-UA-70072493-5" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
+      <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=UA-70072493-5" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
       <style jsx global>{`
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;800;900&display=swap');
 
