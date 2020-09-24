007087893ef1dd107d1d743136eef7dc765b3d55
T-Alexander <tomalexander404@gmail.com>
2020-09-24 15:41:12 +0100

    ga tweak

--- components/Layout.js
+++ components/Layout.js
@@ -25,15 +25,11 @@ export default function Layout({ children, pageTitle, description, ...props }) {
           }}
         /> */}
 
-<script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','UA-70072493-5');`}} />
-
+<script dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-70072493-5');`}} />
+<script src="https://www.googletagmanager.com/gtag/js?id=UA-70072493-5" async></script>
         <title>{pageTitle}</title>        
       </Head>
-      <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=UA-70072493-5" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
+      
       <style jsx global>{`
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700;800;900&display=swap');
 
